027925502262c1613f1c2d674217cea21612f42c
Jerry Jiang <jerryjiangggg@gmail.com>
2025-01-27 22:26:03 -0600

    Version 3

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main()
 {
     cout << "Hello, I like dorayaki (japanese red bean pancake)" << endl;
+    cout << "My favorite drink is pocari sweat" << endl;
 
     return 0;
 }
